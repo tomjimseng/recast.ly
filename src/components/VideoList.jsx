420958ac7b835ae50320c13bd9c61c19c66f289b
tomjimseng <tomjimseng@gmail.com>
2020-02-18 21:36:30 -0800

    Populate Video List with Default

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,21 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+
+var VideoList = (videos) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div>
+      <h5><em>videoListEntry</em>
+
+        {exampleVideoData.map((videos, index) =>
+          <VideoListEntry video={videos[index]} />
+          console.log(videos);
+        )};
+
+      </h5>
+    </div>
+    {/*
+    <div><h5><em>videoListEntry</em> {props.exampleVideoData[1]}</h5></div> */}
+
   </div>
 );
 
@@ -14,6 +25,9 @@ VideoList.propTypes = {
   videos: PropTypes.array.isRequired
 };
 
+// ReactDOM.render(<VideoList />, document.getElementById('app'));
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+// snippet.title, snippet.description, snippet.thumbnails.default.url
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,15 +1,37 @@
-var VideoListEntry = () => (
+import exampleVideoData from '../data/exampleVideoData.js';
+
+var VideoListEntry = (video) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      <img className="media-object" src={video}'https://i.ytimg.com/vi/4ZAEBxGipoA/default.jpg'alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title"></div>
+      <div className="video-list-entry-detail">'My website - https://www.thenewboston.com/videos.php Have questions about the tutorial or React? Ask them here ...'</div>
     </div>
   </div>
+
 );
 
+// class VideoListEntry extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div className="video-list-entry media">
+//         <div className="media-left media-middle">
+//           <img className="media-object" src='snippet.thumbnail.default.url' alt="" />
+//         </div>
+//         <div className="media-body">
+//           <div className="video-list-entry-title">snippet.title</div>
+//           <div className="video-list-entry-detail">snippet.description</div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoListEntry.propTypes = {
